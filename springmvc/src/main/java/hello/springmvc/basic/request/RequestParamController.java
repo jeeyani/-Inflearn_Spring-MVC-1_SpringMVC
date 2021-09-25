7c4e801ef5de50ee4f867dc1ba54faffdfaec980
jeeyani <74583344+jeeyani@users.noreply.github.com>
2021-09-25 18:14:39 +0900

    스프링 MVC 1편 - 스프링 MVC(기본 기능).HTTP요청 파라미터@RequestParam

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.Map;
 
 @Slf4j
+//@RestController
 @Controller
 public class RequestParamController {
 
@@ -19,9 +20,76 @@ public class RequestParamController {
 	public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String username = request.getParameter("username");
 		int age = Integer.parseInt(request.getParameter("age"));
-		
+
 		log.info("username={}, age={}", username, age);
-		
+
 		response.getWriter().write("ok");
+	}
+
+	@ResponseBody //응답메시지에 바로 넣어버림 >> view 랜더링 진행 하지 않음
+	@RequestMapping("/request-param-v2")
+	public String requestParamV2(@RequestParam("username") String memberName, @RequestParam("age") int memberAge) {
+		log.info("username={}, age={}", memberName, memberAge);
+		return "ok";
+
+	}
+
+	@ResponseBody
+	@RequestMapping("/request-param-v3")
+	public String requestParamV3(@RequestParam String username, @RequestParam int age) {
+		log.info("username={}, age={}", username, age);
+		return "ok";
+
+	}
+
+	@ResponseBody
+	@RequestMapping("/request-param-v4")
+	public String requestParamV4(String username, int age) //요청파라미터와 이름이 동일하면 어노테이션도 삭제 가능~!
+	{
+		log.info("username={}, age={}", username, age);
+		return "ok";
+
+	}
+
+	/**
+	 * @RequestParam.required /request-param -> username이 없으므로 예외
+	 *
+	 *                        주의! /request-param?username= -> 빈문자로 통과
+	 *
+	 *                        주의! /request-param int age -> null을 int에 입력하는 것은 불가능,
+	 *                        따라서 Integer 변경해야 함(또는 다음에 나오는 defaultValue 사용)
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-required")
+	public String requestParamRequired(@RequestParam(required = true) String username, //required = true 무조건 들어와야하는 값
+			@RequestParam(required = false) Integer age) {
+		log.info("username={}, age={}", username, age);
+		return "ok";
+	}
+
+	/**
+	 * @RequestParam - defaultValue 사용
+	 *
+	 *               참고: defaultValue는 빈 문자의 경우에도 적용 /request-param?username=
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-default")
+	public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username,
+			@RequestParam(required = false, defaultValue = "-1") int age) {
+		log.info("username={}, age={}", username, age);
+		return "ok";
+	}
+
+	/**
+	 * @RequestParam Map, MultiValueMap Map(key=value) MultiValueMap(key=[value1,
+	 *               value2, ...] ex) (key=userIds, value=[id1, id2])
+	 */
+	@ResponseBody
+	@RequestMapping("/request-param-map")
+	public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+		
+		log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+		return "ok";
+		
 	}
 }
